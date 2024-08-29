b70f3aa1681f7b37bba32d41503bc6f3014e4876
Newriver758 <shinkawa0109@gmail.com>
2024-08-29 19:35:32 +0900

    Users Tableの外部キーカラム記述修正

--- db/migrate/20240822214203_add_user_id_to_prototypes.rb
+++ db/migrate/20240822214203_add_user_id_to_prototypes.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPrototypes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :prototypes, :user, null: false, foreign_key: true
+    add_references :prototypes, :user, null: false, foreign_key: true
   end
 end
